f3efbeda9702e7bb919b4e7d97ae3faf183a5adf
Mitesh Sharma <mitsharm@redhat.com>
2025-09-01 16:41:50 +0530

    update

--- setup-automation/setup-controller.sh
+++ setup-automation/setup-controller.sh
@@ -1,155 +1,36 @@
 #!/bin/bash
 USER=rhel
 
-cat >~/.ssh/private_key <<EOF
------BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAYEAnBQ7FLS6gVjcfZ5mV+2woQYjZ4QlIdAPHhFc2HWKWVW4MSRiSkXa
-DXbhMwF1PM722Wh/q8+y2MFL8F49U4kISXel/pyP5ePJbE1Z8JN2eyUKJ4LJtWx4DJc0KL
-yuxgaF/rJ8QHs4Gwdod22nm92jrqrwat523K3THWpEDJUXOaCWxeOEztl/sm6BUUeLbYUK
-ho5ypIGaxgONjY8ijmAn0YAQctqkiE9g+gv7MwDWGkvnI4JXPzoDwL3GQY5X5QaJ8nie3L
-mCSK5Lv5IEYUPIC9vL2LYKoMmtu4cxaawNIUj1FppucCPKeQFWakqqQOEPj9lhObZ0c1hc
-1umT6XhMJNyvduOZatdLt5yC3/AsJ8y/LKdCXw/ESRSvA7KwVzv7hbvRxbIloBzpm0nwnN
-ijkTOwmCxXPHRgYTv+Fogp9wo+/VSro5N+1dM9p0olJ5AHwapN7zX9sDuM56sC8+TcMHtw
-AT3nuYlvdILjvaizZf89H58fVUHXa3agejkJAUrBAAAFgLRasqa0WrKmAAAAB3NzaC1yc2
-EAAAGBAJwUOxS0uoFY3H2eZlftsKEGI2eEJSHQDx4RXNh1illVuDEkYkpF2g124TMBdTzO
-9tlof6vPstjBS/BePVOJCEl3pf6cj+XjyWxNWfCTdnslCieCybVseAyXNCi8rsYGhf6yfE
-B7OBsHaHdtp5vdo66q8Gredtyt0x1qRAyVFzmglsXjhM7Zf7JugVFHi22FCoaOcqSBmsYD
-jY2PIo5gJ9GAEHLapIhPYPoL+zMA1hpL5yOCVz86A8C9xkGOV+UGifJ4nty5gkiuS7+SBG
-FDyAvby9i2CqDJrbuHMWmsDSFI9RaabnAjynkBVmpKqkDhD4/ZYTm2dHNYXNbpk+l4TCTc
-r3bjmWrXS7ecgt/wLCfMvyynQl8PxEkUrwOysFc7+4W70cWyJaAc6ZtJ8JzYo5EzsJgsVz
-x0YGE7/haIKfcKPv1Uq6OTftXTPadKJSeQB8GqTe81/bA7jOerAvPk3DB7cAE957mJb3SC
-472os2X/PR+fH1VB12t2oHo5CQFKwQAAAAMBAAEAAAGAMjeYIrtbXo6WspwqVZQWRljRP1
-cXE1/73TK86rA2yTN5ReZs8XS0Caz8HAsWC3CTn7OtkDwNUQwwQLAxjxUB45FpiRgafF7j
-ycLBc1QexNRQBhJgj8zMe1CS6aB7ox8qMZ5/t8dtb9TcWcCgMtgG+AXrwJa/K5WJ3Wb5ag
-r2JAZ1PEpNyrfUDDbdyKb6R0yM7GocnK1OfPNbsfYhqilp5lAL+sg6M9lGXG1Kb0iPmnpd
-beFb7O3fkz378c+4m+mULBDaH0BZ/56TN8AoWWDw0kY9Kr3qNgmGMtDEoW2520mu4XVozM
-UB0rU7jVCv15IMrbscRdvxdlCI7ybeePtZe0kehVGJKO86s6KUI5L8TZTzn51aFz9wXrsk
-qTqPvVW/1KDdKdF8D2ssjlkHIP42/r+nJmapwcK2K4QbyA0tzPcIOz2CIwguzNEkwD+6iF
-lp+Vww6vPJzzj6cLz2GqLxxZFVmdvytVCDNLrr6SV5GNUfTgpM2DO8z5PUsEekmdXZAAAA
-wQCZ/qY6/W4e4AJrO00zhuVIgPTgCDqyPDGhG3aExcu1s5fRsXY5wCcC1470WU8p9etgeN
-o6hkv3Nu5Jp7v43+LRxNGDKu81MP1eXfmjXWA8AYixGS1mQVh6YU3Lo/8ZNrkgFn42kMC3
-rro1IZpBID4sK57frlok2yN3Jd8YbbH5DUJdJAXH9nQtIWd/+rzITpB9+INceNGLYp2ZVi
-I948kpjZgc1DtYOYDSpQ3NflDvDz57DBJcJdtMlZI0dhQJZs0AAADBAM1QYmeOeS49wau1
-X8zF5vLiksHi4ibNyvo2W0jMpTbqoDukMdmfGnfRB3tGoej+77Pa5cMDALKnBdj4Q+aKxu
-V8Dxt9Xgca2WZ7EAy3uiPAh0OWvn7KE16kEt8tGHA5NpE4Cc5kuhVB64uhTk5Pg9isTWWq
-Aqwhm9rjN3zjNCB/jEpTWYvJQiKhKONeHUdld6S+2ljVxOWeUlJTcT682wQGDoZ8sGsw8z
-lLRS20S+NJ4lQcXsyrmR3+A0WSUWmbSwAAAMEAwpxAPo2gqzkq127Kx86jHcrsyORvWtV+
-a3gIlT/oyhqbuB1HOJmJw8SXpm922GgbWHzqg/r4LlLk0vd8D1Z73gBUCPBKdnPTMI24+P
-bvcn01vhMLQzNpZ5e8He09SPf5Zgv0Ue/8ffij9VF+d44aDTDNA/a1r0bMOCH05zAYXVe3
-dIH4wTK/LTw2NVXKowB0cmm2RlwVsUE0vCa0XLwLRMA5+7gm8wIPoRnXq+86u1SK6CS5K4
-6t9RQiSpLLrf6jAAAAB2Fuc2libGUBAgM=
------END OPENSSH PRIVATE KEY-----
-EOF
-
-chmod 600 ~/.ssh/private_key
-
-cat >~/.ssh/config <<EOF
-Host *
-     StrictHostKeyChecking no
-     User ansible
-     IdentityFile /root/.ssh/private_key
-EOF
-
-tee /home/rhel/hosts << EOF
-cisco ansible_connection=network_cli ansible_network_os=ios ansible_become=true ansible_user=ansible ansible_password=ansible123!
-code-server ansible_user=rhel ansible_password=ansible123!
-EOF
-
-# set vscode default settings
-su - $USER -c 'cat >/home/$USER/.local/share/code-server/User/settings.json <<EOL
-{
-  "git.ignoreLegacyWarning": true,
-  "window.menuBarVisibility": "visible",
-  "git.enableSmartCommit": true,
-  "workbench.tips.enabled": false,
-  "workbench.startupEditor": "readme",
-  "telemetry.enableTelemetry": false,
-  "search.smartCase": true,
-  "git.confirmSync": false,
-  "workbench.colorTheme": "Solarized Dark",
-  "update.showReleaseNotes": false,
-  "update.mode": "none",
-  "ansible.ansibleLint.enabled": false,
-  "ansible.ansible.useFullyQualifiedCollectionNames": true,
-  "files.associations": {
-      "*.yml": "ansible",
-      "*.yaml": "ansible"
-  },
-  "files.exclude": {
-    "**/.*": true
-  },
-  "window.autoDetectColorScheme": true,
-  "security.workspace.trust.enabled": false
-}
-EOL
-cat /home/$USER/.local/share/code-server/User/settings.json'
-
-# set ansible-navigator default settings
-su - $USER -c 'cat >/home/$USER/ansible-navigator.yml <<EOL
+## --------------------------------------------------------------
+## Create sudoers using playbook
+## --------------------------------------------------------------
+cat > /tmp/create_sudoers_user.yml << EOF
 ---
-ansible-navigator:
-  ansible:
-    inventories:
-    - /home/$USER/hosts
-  execution-environment:
-    container-engine: podman
-    image: network-ee
-    enabled: True
-    pull-policy: never
-
-  playbook-artifact:
-    save-as: /home/rhel/playbook-artifacts/{playbook_name}-artifact-{ts_utc}.json
-
-  logging:
-    level: debug
-
-EOL
-cat /home/$USER/ansible-navigator.yml'
-
-# Fixes an issue with podman that produces this error: "Error: error creating tmpdir: mkdir /run/user/1000: permission denied"
-su - $USER -c 'loginctl enable-linger $USER'
-
-# Creates playbook artifacts dir
-su - $USER -c 'mkdir /home/$USER/playbook-artifacts'
-
-# Creates playbook artifacts dir
-su - $USER -c 'mkdir /home/$USER/.ssh'
-
-
-tee /home/rhel/.ssh/config << EOF
-Host *
-     StrictHostKeyChecking no
-     User ansible
+- name: Setup sudoers
+  hosts: localhost
+  become: true
+  gather_facts: false
+  vars:
+    ansible_become_password: ansible123!
+  tasks:
+    - name: Create sudo file
+      copy:
+        dest: /etc/sudoers.d/rhel_sudoers
+        content: "%rhel ALL=(ALL:ALL) NOPASSWD:ALL"
+        owner: root
+        group: root
+        mode: 0440
 EOF
+/usr/bin/ansible-playbook /tmp/create_sudoers_user.yml
+# remove seetup playbook
+rm /tmp/create_sudoers_user.yml
 
-sudo chown rhel:rhel /home/rhel/.ssh/config
 
-tee /home/rhel/ansible.cfg << EOF
-[defaults]
-# stdout_callback = yaml
-connection = smart
-timeout = 60
-deprecation_warnings = False
-devel_warning = False
-action_warnings = False
-system_warnings = False
-host_key_checking = False
-collections_on_ansible_version_mismatch = ignore
-retry_files_enabled = False
-interpreter_python = auto_silent
-[persistent_connection]
-connect_timeout = 200
-command_timeout = 200
-EOF
-
-su - $USER -c 'podman pull quay.io/ansible/network-ee:stable-2.12'
-
-echo 'shopt -s histappend' >> /home/rhel/.bashrc
-echo 'PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> /home/rhel/.bashrc
-
-mkdir /home/rhel/setup-scripts
-tee /home/rhel/setup-scripts/network-setup-survey.yml << EOF
+# --------------------------------------------------------------
+# Setup lab assets
+# --------------------------------------------------------------
+# Write a new playbook to create a template from above playbook
+cat > /home/rhel/setup-scripts/network-setup-survey.yml << EOF
 ---
 - name: setup controller for network use cases
   hosts: localhost
@@ -277,12 +158,94 @@ tee /home/rhel/setup-scripts/network-setup-survey.yml << EOF
         validate_certs: false
 EOF
 
-ansible-playbook /home/rhel/setup-scripts/network-setup-survey.yml
+# ansible-playbook /home/rhel/setup-scripts/network-setup-survey.yml
+
+
+# --------------------------------------------------------------
+# set ansible-navigator default settings
+# --------------------------------------------------------------
+cat >/home/$USER/ansible-navigator.yml <<EOL
+---
+ansible-navigator:
+  ansible:
+    inventories:
+    - /home/$USER/hosts
+  execution-environment:
+    container-engine: podman
+    image: network-ee
+    enabled: True
+    pull-policy: never
+
+  playbook-artifact:
+    save-as: /home/rhel/playbook-artifacts/{playbook_name}-artifact-{ts_utc}.json
+
+  logging:
+    level: debug
+
+EOL
+cat /home/$USER/ansible-navigator.yml
+
+
+# --------------------------------------------------------------
+# create inventory hosts file
+# --------------------------------------------------------------
+cat > /home/rhel/hosts << EOF
+cisco ansible_connection=network_cli ansible_network_os=ios ansible_become=true ansible_user=admin ansible_password=ansible123!
+vscode ansible_user=rhel ansible_password=ansible123!
+EOF
+cat  /home/rhel/hosts
+
+
+# --------------------------------------------------------------
+# set environment
+# --------------------------------------------------------------
+# Fixes an issue with podman that produces this error: "Error: error creating tmpdir: mkdir /run/user/1000: permission denied"
+loginctl enable-linger $USER
 
 # Fix DNS on RHEL9
 echo "search $_SANDBOX_ID.svc.cluster.local." >> /etc/resolv.conf
 
 # Work with old school Cisco SSH
 update-crypto-policies --set LEGACY
+
+# Creates playbook artifacts dir
+mkdir /home/$USER/playbook-artifacts
+
+# podman pull quay.io/ansible/network-ee:stable-2.12
+
+
+# --------------------------------------------------------------
+# configure ssh
+# --------------------------------------------------------------
+# Creates ssh dir
+mkdir /home/$USER/.ssh
+
+tee /home/rhel/.ssh/config << EOF
+Host *
+     StrictHostKeyChecking no
+     User ansible
+EOF
+
+
+# --------------------------------------------------------------
+# create ansible.cfg
+# --------------------------------------------------------------
+tee /home/rhel/ansible.cfg << EOF
+[defaults]
+# stdout_callback = yaml
+connection = smart
+timeout = 60
+deprecation_warnings = False
+action_warnings = False
+system_warnings = False
+host_key_checking = False
+collections_on_ansible_version_mismatch = ignore
+retry_files_enabled = False
+interpreter_python = auto_silent
+[persistent_connection]
+connect_timeout = 200
+command_timeout = 200
+EOF
+
 
 exit 0
